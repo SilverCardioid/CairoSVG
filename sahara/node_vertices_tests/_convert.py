8ed58344b9a93fa6208dfb0cc9d77919755f2bf2
SilverCardioid <sibridband@gmail.com>
2020-11-29 21:48:22 +0100

    Allow regenerating multiple images, or with only some scripts

--- sahara/node_vertices_tests/_convert.py
+++ sahara/node_vertices_tests/_convert.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import importlib.util
 import _show_node_vertices
@@ -9,23 +10,38 @@ def import_url(name, url):
 	spec.loader.exec_module(mod)
 	return mod
 
-image = input('Input image: ')
-if image[-4:] != '.svg': image += '.svg'
-
+image = input('Input image(s): ')
+scripts = input('Scripts [iksv]: ').lower() or 'iksv'
+if not image:
+	images = glob.glob('*.svg')
+else:
+	images = image.split('|')
+	for i,image in enumerate(images):
+		if image[-4:] != '.svg':
+			images[i] += '.svg'
+	
 repo_base = '../../'
 
+## Inkscape
+if 'i' in scripts:
+	for image in images:
+		os.system('inkscape --export-png="' + os.path.splitext(image)[0] + '-ink.png" ' + image + '"')
+
 ## CairoSVG master branch (synced with Kozea/CairoSVG)
-cairosvg = import_url('cairosvg', repo_base + '../CairoSVG-kozea/cairosvg/__init__.py')
-out = os.path.splitext(image)[0] + '-kozea.png'
-cairosvg.svg2png(url=image, write_to=out)
+if 'k' in scripts:
+	for image in images:
+		cairosvg = import_url('cairosvg', repo_base + '../CairoSVG-kozea/cairosvg/__init__.py')
+		out = os.path.splitext(image)[0] + '-kozea.png'
+		cairosvg.svg2png(url=image, write_to=out)
 
 ## CairoSVG main branch
-parse = import_url('cairosvg', repo_base + 'cairosvg/parse/__init__.py')
-out = os.path.splitext(image)[0] + '-agc.png'
-parse.svg2png(url=image, write_to=out)
+if 's' in scripts:
+	for image in images:
+		parse = import_url('cairosvg', repo_base + 'cairosvg/parse/__init__.py')
+		out = os.path.splitext(image)[0] + '-agc.png'
+		parse.svg2png(url=image, write_to=out)
 
 ## CairoSVG, visualise node.vertices
-_show_node_vertices.main(image)
-
-## Inkscape
-os.system('inkscape --export-png="' + os.path.splitext(image)[0] + '-ink.png" ' + image + '"')
+if 'v' in scripts:
+	for image in images:
+		_show_node_vertices.main(image)
